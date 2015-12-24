e13135135d34026c25536e75d16615aa2af90b5e
starter <starter@CLASS84.training.local>
2015-12-24 10:45:39 +0200

    commit

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-HEAD
         System.out.println( "Hello World! Hello" );
-
-        System.out.println( "Hello World!!!" );
-branch 'master' of https://github.com/softservedata/atf.git
     }
 }
